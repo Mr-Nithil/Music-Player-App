b3debccb613526a87c322b39ad790c990d2ad517
Mr-Nithil <nithilsheshan@gmail.com>
2026-04-20 21:20:19 +0530

    refactor database session handling and update signup user function to use dependency injection

--- server/routes/auth.py
+++ server/routes/auth.py
@@ -1,17 +1,18 @@
 import uuid
 
 import bcrypt
-from fastapi import HTTPException
+from fastapi import Depends, HTTPException
 
 from server.models.user import User
 from server.pydantic_schemas.user_create import UserCreate
 from fastapi import APIRouter
-from server.database import db
+from server.database import get_db
+from sqlalchemy.orm import Session
 
 router = APIRouter()
 
 @router.post('/signup')
-def signup_user(user: UserCreate):
+def signup_user(user: UserCreate, db: Session=Depends(get_db)):
     # check if the user already exist in the db
     user_db = db.query(User).filter(User.email == user.email).first()
 
