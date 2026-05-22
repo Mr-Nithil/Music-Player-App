2961ef7f9a11942f66b97f9657deb82aa40a70af
Mr-Nithil <nithilsheshan@gmail.com>
2026-05-22 16:07:21 +0530

    feat: implement SongsPage layout with song thumbnails and details

--- client/lib/features/home/view/pages/songs_page.dart
+++ client/lib/features/home/view/pages/songs_page.dart
@@ -1,3 +1,6 @@
+import 'package:client/config/theme/color_palette.dart';
+import 'package:client/core/widgets/loader.dart';
+import 'package:client/features/home/viewmodel/home_viewmodel.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -11,6 +14,84 @@ class SongsPage extends ConsumerStatefulWidget {
 class _SongsPageState extends ConsumerState<SongsPage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return SafeArea(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(
+              "Latest today",
+              style: TextStyle(fontSize: 23, fontWeight: FontWeight.w700),
+            ),
+          ),
+          ref
+              .watch(getAllSongsProvider)
+              .when(
+                data: (songs) {
+                  return SizedBox(
+                    height: 260,
+                    child: ListView.builder(
+                      scrollDirection: Axis.horizontal,
+                      itemCount: songs.length,
+                      itemBuilder: (context, index) {
+                        final song = songs[index];
+                        return Padding(
+                          padding: const EdgeInsets.only(left: 16),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Container(
+                                width: 180,
+                                height: 180,
+                                decoration: BoxDecoration(
+                                  image: DecorationImage(
+                                    image: NetworkImage(song.thumbnail_url),
+                                    fit: BoxFit.cover,
+                                  ),
+                                  borderRadius: BorderRadius.circular(8),
+                                ),
+                              ),
+                              SizedBox(height: 5),
+                              SizedBox(
+                                width: 180,
+                                child: Text(
+                                  song.song_name,
+                                  style: TextStyle(
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.w700,
+                                    overflow: TextOverflow.ellipsis,
+                                  ),
+                                  maxLines: 1,
+                                ),
+                              ),
+                              SizedBox(
+                                width: 180,
+                                child: Text(
+                                  song.artist,
+                                  style: TextStyle(
+                                    color: ColorPalette.subtitleText,
+                                    fontSize: 13,
+                                    fontWeight: FontWeight.w500,
+                                    overflow: TextOverflow.ellipsis,
+                                  ),
+                                  maxLines: 1,
+                                ),
+                              ),
+                            ],
+                          ),
+                        );
+                      },
+                    ),
+                  );
+                },
+                error: (error, st) {
+                  return Center(child: Text(error.toString()));
+                },
+                loading: () => Loader(),
+              ),
+        ],
+      ),
+    );
   }
 }
